e3f345bf4825000fb9ff3a826f735bccc27624c1
Sophylax <sciasophylax@gmail.com>
2013-09-05 21:44:44 +0300

    Auto Armlet Toggle v1.0b
    - Lowered menu Width
    - Script now disables itself if the user is under Ice Blast effect

--- Scripts/AutoArmletToggle.lua
+++ Scripts/AutoArmletToggle.lua
@@ -11,20 +11,26 @@ require("libs.HotkeyConfig")
  0 1 1 0 0 0 0 1    
  0 1 1 1 1 0 0 0 
 
-			Auto Armlet Toggle  v1.0
+			Auto Armlet Toggle  v1.0a
 
-		This scripts uses armlet to gain hp when your hero is below a specified health.
+		This script uses armlet to gain hp when your hero is below a specified health.
 
 		Changelog:
+			v1.0a:
+			 - Script now disables itself if the user is under Ice Blast effect
+			 - Lowered menu Width
+
 			v1.0:
 			 - Release
 
 ]]
 
 ScriptConfig:SetName("Auto-Armlet")
+ScriptConfig:SetExtention(-.3)
+ScriptConfig:SetVisible(false)
+
 ScriptConfig:AddParam("active","Auto-Armlet",SGC_TYPE_TOGGLE,true,true,string.byte("L"))
 ScriptConfig:AddParam("minhp","Min HP",SGC_TYPE_NUMCYCLE,true,400,nil,100,450,50)
-ScriptConfig:SetVisible(false)
 
 function Tick( tick )
 	if not PlayingGame() then
@@ -43,7 +49,7 @@ function Tick( tick )
 
 	local armState = me:DoesHaveModifier("modifier_item_armlet_unholy_strength")
 
-	if ScriptConfig.active and SleepCheck() then
+	if not me:DoesHaveModifier("modifier_ice_blast") and ScriptConfig.active and SleepCheck() then
 		if me.health <= ScriptConfig.minhp + 0 then
 			if armState then
 				me:SafeCastItem("item_armlet")
