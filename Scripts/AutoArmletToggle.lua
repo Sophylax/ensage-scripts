40a33c2ea0988042e8efed5f9c1f339e53e45c08
Sophylax <sciasophylax@gmail.com>
2013-09-26 22:50:49 +0300

    Auto Armlet Toggle  v1.0c
    - Tweaked script for the new armlet mechanics
    - Added key for manual armlet toggling

--- Scripts/AutoArmletToggle.lua
+++ Scripts/AutoArmletToggle.lua
@@ -11,11 +11,15 @@ require("libs.HotkeyConfig")
  0 1 1 0 0 0 0 1    
  0 1 1 1 1 0 0 0 
 
-			Auto Armlet Toggle  v1.0b
+			Auto Armlet Toggle  v1.0c
 
 		This script uses armlet to gain hp when your hero is below a specified health.
 
 		Changelog:
+			v1.0c:
+			 - Tweaked script for the new armlet mechanics
+			 - Added key for manual armlet toggling
+
 			v1.0b:
 			 - Script now checks armlet cooldown even if it is activated by the user
 
@@ -34,6 +38,9 @@ ScriptConfig:SetVisible(false)
 
 ScriptConfig:AddParam("active","Auto-Armlet",SGC_TYPE_TOGGLE,true,true,string.byte("L"))
 ScriptConfig:AddParam("minhp","Min HP",SGC_TYPE_NUMCYCLE,true,400,nil,100,450,50)
+ScriptConfig:AddParam("manual","Manual-Armlet",SGC_TYPE_ONKEYDOWN,false,false,string.byte("B"))
+
+extraToggle = 0
 
 function Tick( tick )
 	if not PlayingGame() then
@@ -52,13 +59,24 @@ function Tick( tick )
 
 	local armState = me:DoesHaveModifier("modifier_item_armlet_unholy_strength")
 
-	if not me:DoesHaveModifier("modifier_ice_blast") and ScriptConfig.active and armlet.cd == 0 and SleepCheck() then
-		if me.health <= ScriptConfig.minhp + 0 then
+	if not me:DoesHaveModifier("modifier_ice_blast") and (ScriptConfig.active or ScriptConfig.manual) and armlet.cd == 0 and SleepCheck() then
+		if me.health <= ScriptConfig.minhp + 0 or ScriptConfig.manual then
 			if armState then
-				me:SafeCastItem("item_armlet")
+				extraToggle = 2
+			else
+				extraToggle = 1
 			end
-			me:SafeCastItem("item_armlet")
 			Sleep(1000)
+		end
+	end
+
+	if extraToggle > 0 and SleepCheck("toggle") then
+		print(tick)
+		if armlet.state == STATE_READY and armlet.cd == 0 then
+			if me:SafeCastItem("item_armlet") then
+				extraToggle = extraToggle - 1
+				Sleep(85,"toggle")
+			end
 		end
 	end
 end
