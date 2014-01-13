bf9c550d91b4b44793dadc1b25a4c083dd488ec1
Sophylax <sciasophylax@gmail.com>
2014-01-13 21:14:38 +0200

    Auto Armlet Toggle v1.1
    - Tweaked script for the new armlet mechanics again
    - Removed the configurable armlet delay

--- Scripts/AutoArmletToggle.lua
+++ Scripts/AutoArmletToggle.lua
@@ -11,11 +11,15 @@ require("libs.HotkeyConfig")
  0 1 1 0 0 0 0 1    
  0 1 1 1 1 0 0 0 
 
-			Auto Armlet Toggle  v1.0c
+			Auto Armlet Toggle  v1.1
 
 		This script uses armlet to gain hp when your hero is below a specified health.
 
 		Changelog:
+			v1.1:
+			 - Tweaked script for the new armlet mechanics again
+			 - Removed the configurable armlet delay
+
 			v1.0c:
 			 - Tweaked script for the new armlet mechanics
 			 - Added key for manual armlet toggling
@@ -31,7 +35,7 @@ require("libs.HotkeyConfig")
 			 - Release
 
 ]]
-
+ScriptConfig = ConfigGUI:New(script.name)
 ScriptConfig:SetName("Auto-Armlet")
 ScriptConfig:SetExtention(-.3)
 ScriptConfig:SetVisible(false)
@@ -39,6 +43,8 @@ ScriptConfig:SetVisible(false)
 ScriptConfig:AddParam("active","Auto-Armlet",SGC_TYPE_TOGGLE,true,true,string.byte("L"))
 ScriptConfig:AddParam("minhp","Min HP",SGC_TYPE_NUMCYCLE,true,400,nil,100,450,50)
 ScriptConfig:AddParam("manual","Manual-Armlet",SGC_TYPE_ONKEYDOWN,false,false,string.byte("B"))
+
+ARMLET_DELAY = 1000
 
 extraToggle = 0
 
@@ -60,22 +66,20 @@ function Tick( tick )
 	local armState = me:DoesHaveModifier("modifier_item_armlet_unholy_strength")
 
 	if not me:DoesHaveModifier("modifier_ice_blast") and (ScriptConfig.active or ScriptConfig.manual) and armlet.cd == 0 and SleepCheck() then
-		if me.health <= ScriptConfig.minhp + 0 or ScriptConfig.manual then
+		if me.health <= ScriptConfig.minhp + 0 or (ScriptConfig.manual and armState) then
 			if armState then
 				extraToggle = 2
 			else
 				extraToggle = 1
 			end
-			Sleep(1000)
+			Sleep(ARMLET_DELAY)
 		end
 	end
 
-	if extraToggle > 0 and SleepCheck("toggle") then
-		print(tick)
+	if extraToggle > 0 then
 		if armlet.state == STATE_READY and armlet.cd == 0 then
 			if me:SafeCastItem("item_armlet") then
 				extraToggle = extraToggle - 1
-				Sleep(85,"toggle")
 			end
 		end
 	end
